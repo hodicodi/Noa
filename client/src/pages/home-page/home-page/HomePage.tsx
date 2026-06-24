e4f33e877f2b0c6ecb880610e57d9f5b124b1bb8
hodicodi <123hgoldner@gmail.com>
2026-06-24 17:40:35 +0300

    Remove public folder

--- client/src/pages/home-page/home-page/HomePage.tsx
+++ client/src/pages/home-page/home-page/HomePage.tsx
@@ -3,7 +3,6 @@ import NavBar from "../../../components/nav-bar/navBar.tsx";
 import LastPlaylists from "../../../components/recent-playlist/RecentPlaylists.tsx";
 import SuggestedPlaylists from "../../../components/suggested-playlist/suggestedPlaylists.tsx";
 import style from "../home-page/homePage.style.ts";
-import SongDrawer from "../../../components/song-drawer/songDrawer.tsx";
 
 
 const HomePage: React.FC = () => {
--- client/src/pages/home-page/playlist-page/PlaylistPage.tsx
+++ client/src/pages/home-page/playlist-page/PlaylistPage.tsx
@@ -2,19 +2,19 @@ import { Box } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import { FC } from "react";
-import playlistInfo from "../../../../../shared/hardCodedInfo";
-import PlaylistPicture from "../../../components/playlist-picture/PlaylistPicture";
-import SongsInPlaylist from "../../../components/song-in-playlist/SongsInPlaylist";
-import Styles from "./playlistPage.style";
+import playlistInfo from "@shared/hardCodedInfo.ts";
+import PlaylistPicture from "../../../components/playlist-picture/PlaylistPicture.tsx";
+import SongsInPlaylist from "../../../components/song-in-playlist/SongsInPlaylist.tsx";
+import Styles from "./playlistPage.style.ts";
 
 
 const PlaylistPage: FC = () => {
   return (
     <Box sx={Styles.playlistPage}>
             <PlaylistPicture
-            name={playlistInfo[0].name}
-            avaterPicture={playlistInfo[0].avaterPicture}
-            artist={playlistInfo[0].artist}
+            name={(playlistInfo as any)[0].name}
+            avaterPicture={(playlistInfo as any[0]).avaterPicture}
+            artist={(playlistInfo as any).artist}
             />
             <SongsInPlaylist/>
     </Box>
