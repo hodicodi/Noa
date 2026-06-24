071fd95b600e0a015afe539b7ccee51fbb9763cc
hodicodi <116430264+hodicodi@users.noreply.github.com>
2026-06-24 20:29:15 +0300

    Apply suggestion from @nadaver0807
    Co-authored-by: nadaver0807 <153559394+nadaver0807@users.noreply.github.com>

--- client/src/api/services/albumService.ts
+++ client/src/api/services/albumService.ts
@@ -7,6 +7,6 @@ const API = axios.create({ baseURL: 'http://localhost:3000/' });
 export const albumService = {
   getAlbumById: async (albumData: AlbumParams): Promise<AlbumRes> => {
   const response = await API.get<AlbumRes>(`/albums/${albumData.uuid}`, { params: {uuid: albumData.uuid}});
-  return response.data;
+  return response?.data?.album;
   }
 };
